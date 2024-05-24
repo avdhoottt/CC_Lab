63f67060e3f90afe1aab5325037353cb763b6392
Avdhoot Fulsundar <avdhootfulsundar@gmail.com>
2024-05-24 11:39:20 +0530

    changes

--- main.py
+++ main.py
@@ -41,3 +41,22 @@ app = webapp2.WSGIApplication([
     ('/about', AboutHandler),
     ('/contact', ContactHandler)
 ], debug=True)
+
+#  import webapp2
+#  class MainHandler(webapp2.RequestHandler):
+#      def get(self):
+#           self.response.write("Hello World")
+# app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+# 
+# app.yml 
+# runtime: python27
+# api_version: 1
+# threadsafe: False
+
+# handlers:
+# - url: /
+#   script: test.app
+
+
+
+
